2ac613cf1b7ec783163cd4d2aa122fbb4c382b80
Hernan <waza.mango@gmail.com>
2021-01-07 21:31:22 -0300

    second js with if statment

---
+++ script2.js
@@ -0,0 +1,18 @@
+"use strict"
+
+if (4==9){
+    console.log("Error");
+} else if(4==4){
+    console.log("OK!")
+}
+
+
+const num = 50;
+
+if (num < 49){
+    console.log("Error");
+} else if (num > 100){
+    console.log("to much");
+} else {
+    console.log("ok!");
+}
