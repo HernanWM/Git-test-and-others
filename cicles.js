4d5e96a44e79a6836f2b8aff91f53f935c52986f
Hernan <waza.mango@gmail.com>
2021-01-07 22:00:55 -0300

    added cicle js file

---
+++ cicles.js
@@ -0,0 +1,23 @@
+"use strict"
+
+/*let num = 50;
+
+while(num <= 55){
+    console.log(num);
+    num++;
+}
+
+do{
+  console-log(num);
+  num++;
+}
+while(num <= 55);*/ 
+
+for(let i = 1; i<8; i++){
+    //condicion para parar el ciclo previamente
+    /* if(i===6){
+        break;
+    }*/ 
+
+    console.log(i);
+}
