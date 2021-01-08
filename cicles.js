75a1f72e40cc975792c626ee51d546ca26d8006d
Hernan <waza.mango@gmail.com>
2021-01-08 11:21:37 -0300

    test comit 2

--- cicles.js
+++ cicles.js
@@ -24,3 +24,16 @@ for(let i = 1; i<8; i++){
 
     console.log(i);
 }
+
+for(let i = 1; i<8; i++){
+    //condicion para parar el ciclo previamente
+    /* if(i===6){
+        break;
+    }*/ 
+
+    //permite pasar de largo el paso especificado
+    /*continue;*/
+
+    console.log(i);
+}
+
